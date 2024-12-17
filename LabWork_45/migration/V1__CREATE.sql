e5a2f135edd15611a1eacea01b33ef8256023153
Lizaventa <kev05072004@gmail.com>
2024-12-17 23:45:35 +0300

    added docker

--- LabWork_45/migration/V1__CREATE.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE labwork."user" (
-	id uuid NOT NULL,
-	login varchar NOT NULL,
-	"password" varchar NOT NULL,
-	username varchar NOT NULL,
-	"role" varchar NULL,
-	refresh_token varchar NULL,
-	CONSTRAINT user_unique UNIQUE (login)
-);
